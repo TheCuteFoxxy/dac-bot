7c01ef0184bab5b7a575eafc426e1a8b40a42f2c
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-07-27 15:33:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong! `' + `${Date.now() - message.createdTimestamp}` + ' ms`');'
+                    message: 'Pong! `${message.createdTimestamp - Date.now()}`;
                 });
             break;
 			// \hello
