4dc644cc5c178ef6cc10aadf17c513392b78df2c
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-07-30 21:24:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,25 +55,6 @@ if (message.content === '>ping everyone') {
 if (message.content === '>owner') {
     message.channel.send('The Bot Owner Is: <@548009285892833280>');
 
-});
-
-client.login(auth.token);
-
- 
- if (message.content === '>avatar') {
-      message.reply(message.author.avatarURL);
-  }
-  if (message.content === '>help') {
-    message.channel.send('**Commands:** \n `help` - This command \n `ping` - Provides the bots latency ping time \n `avatar` - Provides a link to the users avatar \n `ban reasons` - provides a valid list of ban reasons for DAC');
-  }
-if (message.content === '>ban reasons') {
-     message.channel.send('**Bannable Offenses:** \n `Raiding (With proof)` \n `Hacking (With proof)` \n `Spamming DMs (With proof)` \n `DM Advertising (With proof)` \n `Violating Discord ToS (With proof)` \n `Making Threats (With proof)` \n `Sending Malicious Links (With proof)` \n `Nuking (With proof)` \n `Stealing our bots code (With proof)` \n `Violating Copyright (With proof)` \n `False Reporting` \n `DDoS Attacks (With proof)`');
-}
-if (message.content === '>ping everyone') {
-    message.channel.send('@everyone');
-}
-if (message.content === '>owner') {
-    message.channel.send('The Bot Owner Is: <@548009285892833280>');
 }
 });
 
