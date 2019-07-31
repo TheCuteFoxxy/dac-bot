dcc5e2c52d976fee774ab423419a152a0305cd32
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-07-30 21:03:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,24 +37,25 @@ if (message.content === '>owner') {
   if (!message.guild) return;
 
   // If the message content starts with ">kick"
-  if (message.content.startsWith('>kick')) {
+ if (message.content.startsWith('>kick')) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
       if (member) {
-        member.kick('Optional reason that will display in the audit logs').then(() => {
+        member.kick('null').then(() => {
           message.reply(`Successfully kicked ${user.tag}`);
         }).catch(err => {
           message.reply('I was unable to kick the member');
-          console.error(err);
+          console.error(err)
+return;
         });
-    }
-      else {
-        message.reply('That user isn\'t in this guild!');
-    } 
-      else {
-      message.reply('You didn\'t mention the user to kick!');
-    }
+ } else if(!member){
+        message.reply('That user isn\'t in this guild!')
+return;
+} else if(!user){
+      message.reply('You didn\'t mention the user to kick!')
+return;
+}
   }
 });
 
