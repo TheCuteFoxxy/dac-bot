f86c12c69e0513d25d59ed285c2acccb3fce33ba
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-08-15 16:58:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require("./auth.json");
+const prefix = ">"
 
 
 client.on('ready', () => {
@@ -9,42 +10,69 @@ client.on('ready', () => {
 client.on("ready", async () => {
     console.log(`${client.user.username} Restarting`);
       let setStatus = setInterval(function () {
-          var names = [">help",`DAC`, 'you...'];
+          var names = [">help",`DAC`, 'you'];
           var game = names[Math.floor(Math.random() * names.length)];
          client.user.setActivity(game,{ type: 'watching' });
           client.user.setStatus(`online`);
        }, 10000)
   });
-client.on('message', message => {
-  if (message.content === '>ping') {
-    message.channel.send('Pong!'); message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
-  }
-  if (message.content === '>test embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('Test')
-      // Set the color of the embed
-      .setColor(0xFF0000)
-      // Set the main content of the embed
-      .setDescription('This is a test');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  }
- if (message.content === '>avatar') {
+  client.on('message', message => {
+    if (message.content === prefix + 'ping') {
+        const staff = message.guild.roles.get("569313373720674315")
+        message.delete(1)
+        if(!message.member.roles.has(staff.id)){
+            message.channel.send ('Access Denied')
+            return;
+        } else if(message.member.roles.has(staff.id)){
+          message.channel.send('Pong!'); message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+        }}
+        if (message.content === prefix + 'avatar') {
       message.reply(message.author.avatarURL);
   }
-  if (message.content === '>help') {
-    message.channel.send('**Commands:** \n `help` - This command \n `ping` - Provides the bots latency ping time \n `avatar` - Provides a link to the users avatar \n `ban reasons` - provides a valid list of ban reasons for DAC');
+  if (message.content === prefix + 'help') {
+    message.channel.send('Help command currently unavailable')
   }
 if (message.content === '>ban reasons') {
      message.channel.send('**Bannable Offenses:** \n `Raiding (With proof)` \n `Hacking (With proof)` \n `Spamming DMs (With proof)` \n `DM Advertising (With proof)` \n `Violating Discord ToS (With proof)` \n `Making Threats (With proof)` \n `Sending Malicious Links (With proof)` \n `Nuking (With proof)` \n `Stealing our bots code (With proof)` \n `Violating Copyright (With proof)` \n `False Reporting` \n `DDoS Attacks (With proof)`');
 }
-if (message.content === '>owner') {
+if (message.content === prefix + 'everyone') {
+  const management = message.guild.roles.get("569313188366123010")
+  message.delete(1)
+  if(!message.member.roles.has(management.id)){
+      message.channel.send ('Access Denied')
+      return;
+  } else if(message.member.roles.has(management.id)){
+    message.channel.send('@everyone');
+  }
+}
+if (message.content === prefix + 'owner') {
     message.channel.send('The Bot Owner Is: <@548009285892833280>');
 }
+if (message.content === prefix + 'DAC-158763') {
+  message.delete(1)
+  message.member.addRole("569319986980454400");
+}
+if (message.content === prefix + 'DAC-429214') {
+  message.delete(1)
+  message.member.addRole("569319986980454400");
+}
+if (message.content === prefix + 'DAC-789213') {
+  message.delete(1)
+  message.member.addRole("569319986980454400");
+}
+if (message.content === prefix + 'dm') {
+  message.author.send("DM Successful");
+}
+});
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'member-logs');
+  if (!channel) return;
+  channel.send(`${member}` + 'Joined the server.');
+});
+client.on('guildMemberRemove', member => {
+const channel = member.guild.channels.find(ch => ch.name === 'member-logs');
+if (!channel) return;
+channel.send(`${member}` + 'Left the server');
 });
 
 client.login(auth.token);
