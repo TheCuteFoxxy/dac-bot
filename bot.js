ba8c5a563b07f5c10a37f56d24c469b306f0fb60
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-08-19 17:55:22 -0400

    Push update ---> Add DM on Join
    Added DM on Join to replace "Dyno" join message

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require("./auth.json");
 const prefix = ">"
+const guild = "569304035472179200"
 
 
 client.on('ready', () => {
@@ -21,7 +22,7 @@ client.on("ready", async () => {
         const staff = message.guild.roles.get("569313373720674315")
         message.delete(1)
         if(!message.member.roles.has(staff.id)){
-            message.channel.send ('Access Denied')
+            message.channel.send ('You must be staff to use this command!')
             return;
         } else if(message.member.roles.has(staff.id)){
           message.channel.send('Pong!'); message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
@@ -29,6 +30,7 @@ client.on("ready", async () => {
         if (message.content === prefix + 'avatar') {
       message.reply(message.author.avatarURL);
   }
+  
   if (message.content === prefix + 'help') {
     message.channel.send('Help command currently unavailable')
   }
@@ -39,7 +41,7 @@ if (message.content === prefix + 'everyone') {
   const management = message.guild.roles.get("569313188366123010")
   message.delete(1)
   if(!message.member.roles.has(management.id)){
-      message.channel.send ('Access Denied')
+      message.channel.send ('You must be Operations Management to use this command!')
       return;
   } else if(message.member.roles.has(management.id)){
     message.channel.send('@everyone');
@@ -63,6 +65,9 @@ if (message.content === prefix + 'DAC-789213') {
 if (message.content === prefix + 'dm') {
   message.author.send("DM 1" || "DM 2" || "DM 3");
 }
+if (message.content === prefix + 'keygen') {
+  message.channel.send("DAC-158763" || "DAC-429214" || "DAC-789213");
+}
 });
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'member-logs');
@@ -73,6 +78,12 @@ client.on('guildMemberRemove', member => {
 const channel = member.guild.channels.find(ch => ch.name === 'member-logs');
 if (!channel) return;
 channel.send(`${member}` + 'Left the server');
+});
+const member = guild.member
+client.on('guildMemberAdd', member => {
+
+member.send(`Welcome ${member} to **Discord Advertising Central**! Please read <#569325072230252546> and <#569341867863441413> for the server rules and advertising guidelines. If you at any time need some help, you can send a message to <@546661574182436864> and a staff member will be glad to assist you! \n\nIf you want to partner, head on over to <#569542837415182343> and read that then ping or DM a Partner Manager and they will guide you from there.\n\nYou can get some roles in <#569335136441729039>.\n\nIf you wish to try out our custom services then head over to <#569539946180771856> \n\n__If you leave the server, all your advertisements will be deleted!__\nThanks for joining!`  +  '\n\n To be verified type  `>DAC-429214`' + ` in <#569320176600743936>`)
+
 });
 
 client.login(auth.token);
