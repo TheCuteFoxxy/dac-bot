f63c6b89f5a0e0a5faa00155b2a620b19b3ef9df
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-08-30 10:30:20 -0400

    Add Embeds

--- bot.js
+++ bot.js
@@ -39,6 +39,24 @@ client.on("ready", async () => {
       message.delete(1)
       message.channel.send({embed});
     }      
+if (message.content === prefix + 'ad rights') {
+const embed = {
+  "color": 16711910,
+  "timestamp": "2019-08-30T14:11:55.093Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/attachments/615548285456744504/615553739784454167/DAC_logo_square.jpg",
+    "text": "DAC © 2019; All Rights Reserved"
+  },
+  "fields": [
+    {
+      "name": "Advertising Rights",
+      "value": "By using DAC services and servers you agree to the following: \n\n - Admin access to your server by our team to investigate an incident if deemed necessary. \n\n - The right to have your server removed off our platform. \n\n- To request audit log access."
+    }
+  ]
+};
+message.delete(1)
+message.channel.send({ embed });
+  }
 if  (message.content === prefix + 'partner req') {
 const embed = {
   "color": 65331,
@@ -56,6 +74,24 @@ const embed = {
     message.delete(1)
     message.channel.send({embed});
 }
+if (message.content === prefix + 'Insurance') {
+const embed = {
+  "color": 16711910,
+  "timestamp": "2019-08-30T14:11:55.093Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/attachments/615548285456744504/615553739784454167/DAC_logo_square.jpg",
+    "text": "DAC © 2019; All Rights Reserved"
+  },
+  "fields": [
+    {
+      "name": "DAC Insurance",
+      "value": "By being a member of DAC and its sister servers you are insured against raids or attacks against your server. Please see the insurance policy terms for details. In the event your claim is approved, we will ban the member on your behalf, report them to discord, and send a representative to assist with rebuilding your server (if needed). This service is free and available to all members."
+    }
+  ]
+};
+message.delete(1)
+message.channel.send({embed});
+}
 if (message.content === prefix + 'links') {
 const embed = {
   "color": 1376511,
@@ -66,13 +102,14 @@ const embed = {
   "thumbnail": {
     "url": "https://cdn.discordapp.com/attachments/615548285456744504/615553739784454167/DAC_logo_square.jpg"
   },
-  "fields": [Bug
+  "fields": [
     {
       "name": "Links",
       "value": "**Our Invite:** [https://discord.gg/ngHhXAa](https://discord.gg/ngHhXAa) \n\n **Punishment Appeal:** [https://forms.gle/p8fDS37ZUZPfVDLs7](https://forms.gle/p8fDS37ZUZPfVDLs7) \n\n **Insurance Policy:** [https://docs.google.com/document/d/1nV8MsEyuOOoeIrkEISKOvbziOEPQgwgcGeZkfkttr-M/edit?usp=sharing](https://docs.google.com/document/d/1nV8MsEyuOOoeIrkEISKOvbziOEPQgwgcGeZkfkttr-M/edit?usp=sharing) \n\n **DAC Insuance Claim:** [https://forms.gle/FYi98bK5kPEVW3RGA](https://forms.gle/FYi98bK5kPEVW3RGA)"
     }
   ]
 };
+message.delete(1)
 message.channel.send({embed});
 }
     if (message.content === prefix + 'ping') {
