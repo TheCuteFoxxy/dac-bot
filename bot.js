ea219a3bc7f50d1bebec5440e65b86e1c0a9de99
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-09-21 21:52:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -259,14 +259,14 @@ if (message.content === 'kill me') {
   message.channel.send(`*kills ${message.author}*`)
   message.delete(1)
 }
-if (message.content === prefix + 'test') {
-message.delete(1)
-    const secondary_guild = client.guilds.get("571870208185335848");
-    const user = message.secondary_guild.member(messsage.mentions.users.first());
-    const test001 = message.secondary_guild.channels.get("625142396937830410");
-  
-    test001.send(`Testing mentions defined ${user}`)
-  }
+//if (message.content === prefix + 'test') {
+//message.delete(1)
+//    const secondary_guild = client.guilds.get("571870208185335848");
+//   const user = message.secondary_guild.member(messsage.mentions.users.first());
+//    const test001 = message.secondary_guild.channels.get("625142396937830410");
+//  
+//    test001.send(`Testing mentions defined ${user}`)
+// }
 // if (message.content === prefix + 'ban reasons') {
 // message.delete(1)
 //     message.channel.send('**Bannable Offenses:** \n `Raiding (With proof)` \n `Hacking (With proof)` \n `Spamming DMs (With proof)` \n `DM Advertising (With proof)` \n `Violating Discord ToS (With proof)` \n `Making Threats (With proof)` \n `Sending Malicious Links (With proof)` \n `Nuking (With proof)` \n `Stealing our bots code (With proof)` \n `Violating Copyright (With proof)` \n `False Reporting` \n `DDoS Attacks (With proof)`');
