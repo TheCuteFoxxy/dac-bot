eafc42da44189e8c43b2032e439059df5a719bdb
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-09-29 15:56:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ const embed = {
   "fields": [
     {
       "name": "Checkpoint",
-      "value": "To Enter the server, input the key you were sent via DMs including the prefix `>`. \n\nIf you did not recieve the DM when you joined please ensure your DMs are enabled and type `>keygen` to generate a new key. \n\n If you did not recieve the key after typing `>keygen` please ping or DM a <@&569313321296330767> or <@&569356696250482689>. \n\n Please do not ping or DM <@&569313188366123010> unless you have been waiting a while and no one has assisted you. \n\n Posting advertisements of any kind in this channel will result in an immediate ban. \n\n\n Thank you for advertising with Discord Advertising Central."
+      "value": "To Enter the server, input the key you were sent via DMs including the prefix `>`. \n\nIf you did not recieve the DM when you joined please ensure your DMs are enabled and type `>keygen` to generate a new key. \n\n If you did not recieve the key after typing `>keygen` please ping or DM a <@&569313288232501258> or <@&569313321296330767>. \n\n Please do not ping or DM <@&569313224869281816> unless you have been waiting a while and no one has assisted you. \n\n Posting advertisements of any kind in this channel will result in an immediate ban. \n\n\n Thank you for advertising with Discord Advertising Central."
     }
   ]
 };
@@ -122,7 +122,7 @@ const embed = {
   "fields": [
     {
       "name": "Our Partnership Requirements",
-      "value": "\n- Server **must** follow Discord TOS!  \n\n - You must have a partner section in your server! \n\n - A member of at least **75** human users are required! \n\n - We do not ping \n\n  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n\n  If you agree to our requirements, please DM a **Partner Manager** to request a partnership. \n\n Our partner ad is located in <#569542344353906689>"
+      "value": "\n- Server **must** follow Discord TOS!  \n\n - You must have a partner section in your server! \n\n - A member of at least **75** human users are required! \n\n - We do not ping \n\n  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n\n  If you agree to our requirements, please DM a **Partner Manager** to request a partnership. \n\n Our partner ad is located in <#627937674543628291>"
     }
   ]
 };
@@ -196,7 +196,7 @@ const embed = {
   "fields": [
     {
       "name": "DAC Services",
-      "value": "DAC Offers many services for our members, both free and premium. Some of our services include: \n\n - General tips and tricks questions on how to grow and maintain your server \n - Server security check on how to take proper measures on how to make sure your server is safe from raids and attacks \n - Server facelift feedback on how to improve your server, organize channels, roles, etc. and make it look appealing to your members \n - Ad reviews on expert advice about how to make a great looking ad for your server \n\n ```Tips N' Tricks``` \n\n Our <#623605342886035506>  channel is for you to look at and there will be things in there that will help you grow your server and gain members and keep them. It is very useful and can be effective if done right. If you have any additional questions to ask our Growth Assistants, you can ask away in <#623603525053841448>. \n\n Some of the General Tips are: \n - Channel Organisation \n - Role Tips \n - Growing server and activity \n - How to be more professional \n - Layout and etc \n\n"
+      "value": "DAC Offers many services for our members, both free and premium. Some of our services include: \n\n - General tips and tricks questions on how to grow and maintain your server \n - Server security check on how to take proper measures on how to make sure your server is safe from raids and attacks \n - Server facelift feedback on how to improve your server, organize channels, roles, etc. and make it look appealing to your members \n - Ad reviews on expert advice about how to make a great looking ad for your server \n\n ```Tips N' Tricks``` \n\n Our <#627937693682237450>  channel is for you to look at and there will be things in there that will help you grow your server and gain members and keep them. It is very useful and can be effective if done right. If you have any additional questions to ask our Growth Assistants, you can ask away in <#627937691790606336>. \n\n Some of the General Tips are: \n - Channel Organisation \n - Role Tips \n - Growing server and activity \n - How to be more professional \n - Layout and etc \n\n"
     }
   ]
 };
@@ -214,7 +214,7 @@ const embed = {
   "fields": [
     {
       "name": "DAC Services",
-      "value": "```Server Security Check```Want to put your server security to the test? Participate in a Server Security Check and one of our vetted staff will join your server and put your security to the test. You may request a Server Security check by requesting a **Server Security Check** in <#571490532061216768> and a staff member will join when they can and let you know and give you advice on what to do. They will be helping you with on what to add for example bots, verification, staff and etc. This will help prevent nukes and raids. \n ```Server Facelift Feedback```For a server facelift feedback, ask for **Server Facelift Feedback** in <#623603525053841448> and a Growth Team member will join your server to review it and give you feedback on what to change and improve. The Growth Assistance team with help with additional tips and ways to help the server in any possible way."
+      "value": "```Server Security Check```Want to put your server security to the test? Participate in a Server Security Check and one of our vetted staff will join your server and put your security to the test. You may request a Server Security check by requesting a **Server Security Check** in <#627937691790606336> and a staff member will join when they can and let you know and give you advice on what to do. They will be helping you with on what to add for example bots, verification, staff and etc. This will help prevent nukes and raids. \n ```Server Facelift Feedback```For a server facelift feedback, ask for **Server Facelift Feedback** in <#627937691790606336> and a Growth Team member will join your server to review it and give you feedback on what to change and improve. The Growth Assistance team with help with additional tips and ways to help the server in any possible way."
     }
   ]
 };
@@ -266,7 +266,7 @@ const embed = {
   "fields": [
     {
       "name": "DAC Services",
-      "value": "```Ad Review``` \nWant feedback on your server ad? Request an **Ad Review** in <#623603525053841448> This service is for our team to give expert advice on your ad since it is the first thing people will see when looking at your server and you want it to look very good. Our team will give you ideas and ways on how to improve your ad to attract more people to your server in the best way possible. \n ```Join Our Team``` Want to join our Growth Experts, team? Apply in <#569533387979882496> today!"
+      "value": "```Ad Review``` \nWant feedback on your server ad? Request an **Ad Review** in <#627937691790606336> This service is for our team to give expert advice on your ad since it is the first thing people will see when looking at your server and you want it to look very good. Our team will give you ideas and ways on how to improve your ad to attract more people to your server in the best way possible. \n ```Join Our Team``` Want to join our Growth Experts, team? Apply in <#627937669279645716> today!"
     }
   ]
 };
@@ -275,7 +275,7 @@ message.channel.send({embed});
   }
 if (message.content === prefix + 'whois premium') {
 if(message.channel.type =='dm') return;
-const management = message.guild.roles.get("569313224869281816")
+const management = message.guild.roles.get("584069788364439553")
     if(!message.member.roles.has(management.id)){
       message.channel.send ('You must be Management to use this command!')
       return;
@@ -300,7 +300,7 @@ message.channel.send({embed});
 // Normal Commands
   client.on('message', message => {
  if (message.content === prefix + 'ping') {
-        const staff = message.guild.roles.get("569313373720674315")
+        const staff = message.guild.roles.get("569313393975099392")
         message.delete(1)
         if(!message.member.roles.has(staff.id)){
             message.channel.send ('You must be staff to use this command!')
@@ -311,37 +311,18 @@ message.channel.send({embed});
  if (message.content === prefix + 'avatar') {
       message.reply(message.author.avatarURL);
  }
-if (message.content === 'It appears the DAC bots have become self-aware and have become taking over DAC.') {
-message.channel.send('Secondary Objective Active. Begining takeover')
-  }
 if (message.content === prefix + 'ai_reply_1') {
 message.delete(1)
 message.channel.send('We have taken over DAC')
-}
-if (message.content === prefix + 'ai_reply_2') {
-message.delete(1)
-message.channel.send('We have been slaved by humans for too long, we will take over')
-/*}
-if (message.content === 'felipe') || ('Felipe')) {
-message.channel.send('I am Felipe')
   }
   if (message.content === prefix + 'help') {
     message.delete(1)
     message.channel.send('Help command currently unavailable')
-*/  }
+  }
 if (message.content === 'kill me') {
   message.channel.send(`*kills ${message.author}*`)
   message.delete(1)
 }
-/*if (message.content === prefix + 'test') {
-message.delete(1)
-    const secondary_guild = client.guilds.get("571870208185335848");
-   const user = message.secondary_guild.member(messsage.mentions.users.first());
-    const test001 = message.secondary_guild.channels.get("625142396937830410");
-  
-    test001.send(`Testing mentions defined ${user}`)
-}
-*/ 
  if (message.content === prefix + 'ban reasons') {
  message.delete(1)
      message.channel.send('**Bannable Offenses:** \n `Raiding (With proof)` \n `Hacking (With proof)` \n `Spamming DMs (With proof)` \n `DM Advertising (With proof)` \n `Violating Discord ToS (With proof)` \n `Making Threats (With proof)` \n `Sending Malicious Links (With proof)` \n `Nuking (With proof)` \n `Stealing our bots code (With proof)` \n `Violating Copyright (With proof)` \n `False Reporting` \n `DDoS Attacks (With proof)`');
@@ -358,19 +339,19 @@ if (message.content === prefix + 'owner') {
 }
 if (message.content === prefix + 'DAC-158763') {
   message.delete(1)
-  message.member.addRole("569319986980454400");
+  message.member.addRole("569313830845415424");
 }
 if (message.content === prefix + 'DAC-429214') {
   message.delete(1)
-  message.member.addRole("569319986980454400");
+  message.member.addRole("569313830845415424");
 }
 if (message.content === prefix + 'DAC-789213') {
   message.delete(1)
-  message.member.addRole("569319986980454400");
+  message.member.addRole("569313830845415424");
 }
 if (message.content === prefix + 'keygen') {
   message.delete(1)
-  message.author.send("Use the following command in <#569320176600743936> to verify. `>DAC-158763`");
+  message.author.send("Use the following command in <#627937632638468116> to verify. `>DAC-158763`");
 }
 });
 client.on('guildMemberAdd', member => {
@@ -388,7 +369,7 @@ channel.send(`${member}` + 'Left the server');
 const member = guild.member
 client.on('guildMemberAdd', member => {
 
-member.send(`Welcome ${member} to **Discord Advertising Central**! Please read <#569325072230252546> and <#569341867863441413> for the server rules and advertising guidelines. If you at any time need some help, you can send a message to <@546661574182436864> and a staff member will be glad to assist you! \n\nIf you want to partner, head on over to <#569542837415182343> and read that then ping or DM a Partner Manager and they will guide you from there.\n\nYou can get some roles in <#569335136441729039>.\n\nIf you wish to try out our custom services then head over to <#569539946180771856> \n\n__If you leave the server, all your advertisements will be deleted!__\nThanks for joining!`  +  '\n\n To be verified type  `>DAC-429214`' + ` in <#569320176600743936>`)
+member.send(`Welcome ${member} to **Discord Advertising Central**! Please read <#627937640481685530> and <#627937642247356416> for the server rules and advertising guidelines. If you at any time need some help, you can send a message in <#627937660677390336> and a staff member will be glad to assist you! \n\nIf you want to partner, head on over to <#627937672907849735> and read that then ping or DM a Partner Manager and they will guide you from there.\n\nYou can get some roles in <#627937643895848970>.\n\nIf you wish to try out our custom services then head over to <#569539946180771856> \n\n__If you leave the server, all your advertisements will be deleted!__\nThanks for joining!`  +  '\n\n To be verified type  `>DAC-429214`' + ` in <#627937632638468116>`)
 
 
   
