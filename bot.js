6d498825502f730fcf49c71de3a63087153c4d81
Hamster-0001 <53378195+Hamster-0001@users.noreply.github.com>
2019-10-29 08:35:38 -0400

    Update Bot.js
    Add Ban Command (Experimental)

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-///Version 1.3.1
+///Version 1.3.0
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require("./auth.json");
@@ -325,8 +325,7 @@ const embed = {
     message.channel.send({embed});
 }
 /*const forbidden = ["Fuck", "fuck", "Fuck", "Shit", "shit", "Shit", "SHIT", "Sh1t", "SH1T", "sh1t", "Bitch", "BITCH", "B1tch", "b1tch", "B1TCH"];
-if (message.author.bot)return;
-if forbidden.some(el=> message.content.toLowerCase().includes(el))
+if (message.content.includes(forbidden)) {
     const embed = {
       "color": 16711680,
       "footer": {
@@ -342,7 +341,56 @@ if forbidden.some(el=> message.content.toLowerCase().includes(el))
     };
         message.delete()
         message.channel.send({embed});
-    }*/
+    }
+    */
+const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+const user = message.mentions.users.first(); // returns the user object if an user mention exists
+const banReason = args.slice(1).join(' '); // Reason of the ban (Everything behind the mention)
+​
+if (message.content === prefix + 'ban')
+// Check if an user mention exists in this message
+if (!user) {
+try {
+// Check if a valid userID has been entered instead of a Discord user mention
+if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
+// If the client (bot) can get a user with this userID, it overwrites the current user variable to the user object that the client fetched
+user = message.guild.members.get(args.slice(0, 1).join(' '));
+user = user.user;
+} catch (error) {
+return message.reply('Couldn\' get a Discord user with this userID!');
+}
+}
+if (user === message.author) return message.channel.send('You can\'t ban yourself!'); // Check if the user mention or the entered userID is the message author himsmelf
+if (!reason) return message.reply('You must provide a ban reason!'); // Check if a reason has been given by the message author
+if (!message.guild.member(user).bannable) return message.reply('I can\'t ban that user!'); // Check if the user is bannable with the bot's permissions
+​
+await message.guild.ban(user) // Bans the user
+​
+const Discord = require('discord.js'); // We need Discord for our next RichEmbeds
+const banConfirmationEmbed = new Discord.RichEmbed()
+.setColor('RED')
+.setDescription(`✅ ${user.tag} has been successfully banned!`);
+message.channel.send({
+embed: banConfirmationEmbed
+}); // Sends a confirmation embed that the user has been successfully banned
+​
+​
+const modlogChannelID = '638716078834319370'; // Discord channel ID where you want to have logged the details about the ban
+if (modlogChannelID.length !== 0) {
+if (!client.channels.get(modlogChannelID )) return undefined; // Check if the modlogChannelID is a real Discord server channel that really exists
+​
+const banConfirmationEmbedModlog = new Discord.RichEmbed()
+.setAuthor(`Banned by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
+.setThumbnail(user.displayAvatarURL)
+.setColor('RED')
+.setTimestamp()
+.setDescription(`**Action**: Ban
+**User**: ${user.username}#${user.discriminator} (${user.id})
+**Reason**: ${reason}`);
+client.channels.get(modlogChannelID).send({
+embed: banConfirmationEmbedModlog
+}); // Sends the RichEmbed in the modlogchannel
+}
 if (message.content === 'https://discord.gg/YQEQAmj') {
 message.delete()
 message.reply('That server is blacklisted in Discord Advertising Central')
